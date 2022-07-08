4795dc4e7bdedb22ba0c4a30c7723bb64068491f
Daniel McKenna <mckenna.daniel15@gmail.com>
2022-07-08 09:22:55 -1000

    finished 2 coding challenges in javascript

---
+++ code_challenge/challenge2.js
@@ -0,0 +1,9 @@
+//Make a simple function called greet that returns the most-famous "hello world!".
+
+//I got this off of code wars maybe my settings are set to easy mode?
+
+let greet = () => {
+  return "hello world!";
+};
+
+console.log(greet());
